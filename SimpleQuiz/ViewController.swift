79bfc2f9f646da38738015f74c1678ddda54bd0d
Özgün Yildiz <ozgun.yildiz@hotmail.de>
2021-05-03 15:09:12 +0200

    UIborder color

--- SimpleQuiz/ViewController.swift
+++ SimpleQuiz/ViewController.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    
+    
+    var countries = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        title = countries[1]
+        
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+        
     }
-
+    
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        
+    }
+    
 
 }
 
