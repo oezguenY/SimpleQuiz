70e74a73db624c4799565ac271bd8494d9a08a70
Özgün Yildiz <ozgun.yildiz@hotmail.de>
2021-05-03 14:57:02 +0200

    Project Start

---
+++ SimpleQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleQuiz
+//
+//  Created by Özgün Yildiz on 03.05.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
